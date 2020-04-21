1f94ca88d7dd324b68bbff2d57c3e1a495e93663
Dewey Dunnington <dewey@fishandwhistle.net>
2020-04-21 10:42:33 -0300

    first commit

---
+++ data-raw/update-s2.R
@@ -0,0 +1,56 @@
+
+library(tidyverse)
+
+# download S2
+source_url <- "https://github.com/google/s2geometry/archive/v0.9.0.zip"
+curl::curl_download(source_url, "data-raw/s2-source.tar.gz")
+untar("data-raw/proj-source.tar.gz", exdir = "data-raw")
+
+# make sure the dir exists
+proj_dir <- list.files("data-raw", "^proj-[0-9.]+", include.dirs = TRUE, full.names = TRUE)
+stopifnot(dir.exists(proj_dir), length(proj_dir) == 1)
+
+# headers can be in subdirectories no problem
+# it might make more sense to have these in inst/include,
+# but I can't get that to work...so they are in src/, with
+# the CXXFLAGS=-I../src
+headers <- tibble(
+  path = list.files(file.path(proj_dir, "include"), "\\.(h|hpp|cpp)$", full.names = TRUE, recursive = TRUE),
+  final_path = str_replace(path, ".*?include/", "src/")
+)
+
+# R doesn't do recursive folders easily
+# so instead we replace "/" with "__"
+# there aren't any headers in subdirs, so we don't need to
+# search + replace in any source files
+source_files <- tibble(
+  path = list.files(file.path(proj_dir, "src"), "\\.(h|hpp|c|cpp)$", full.names = TRUE, recursive = TRUE),
+  final_path = str_replace(path, ".*?src/", "src/") %>%
+    str_replace_all("/", "__") %>%
+    str_replace("src__", "src/")
+) %>%
+  filter(!str_detect(path, "/(apps|tests?)/"))
+
+# clean source dir
+current_source_files <- tibble(path = list.files("src", "\\.(h|hpp|cpp|o)$", full.names = TRUE, recursive = TRUE)) %>%
+  filter(!str_detect(path, "^src/(geoproj-|Rcpp|Makevars|proj_config)"))
+
+unlink(current_source_files$path)
+unlink("src/proj/", recursive = TRUE)
+stopifnot(file.remove(list.dirs("src")[!str_detect(list.dirs("src"), "^src$")]))
+
+
+# create destination dirs
+dest_dirs <- c(
+  headers %>% pull(final_path),
+  source_files %>% pull(final_path)
+) %>%
+  dirname() %>%
+  unique()
+dest_dirs[!dir.exists(dest_dirs)] %>% walk(dir.create)
+
+# copy source files
+stopifnot(
+  file.copy(headers$path, headers$final_path),
+  file.copy(source_files$path, source_files$final_path)
+)
