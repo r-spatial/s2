59955b86621a713825d789be013c620109d4ba6e
Dewey Dunnington <dewey@fishandwhistle.net>
2020-04-21 10:47:48 -0300

    use rcpp

--- data-raw/update-s2.R
+++ data-raw/update-s2.R
@@ -4,18 +4,18 @@ library(tidyverse)
 # download S2
 source_url <- "https://github.com/google/s2geometry/archive/v0.9.0.zip"
 curl::curl_download(source_url, "data-raw/s2-source.tar.gz")
-untar("data-raw/proj-source.tar.gz", exdir = "data-raw")
+unzip("data-raw/s2-source.tar.gz", exdir = "data-raw")
 
 # make sure the dir exists
-proj_dir <- list.files("data-raw", "^proj-[0-9.]+", include.dirs = TRUE, full.names = TRUE)
-stopifnot(dir.exists(proj_dir), length(proj_dir) == 1)
+s2_dir <- list.files("data-raw", "^s2geometry-[0-9.]+", include.dirs = TRUE, full.names = TRUE)
+stopifnot(dir.exists(s2_dir), length(s2_dir) == 1)
 
 # headers can be in subdirectories no problem
 # it might make more sense to have these in inst/include,
 # but I can't get that to work...so they are in src/, with
 # the CXXFLAGS=-I../src
 headers <- tibble(
-  path = list.files(file.path(proj_dir, "include"), "\\.(h|hpp|cpp)$", full.names = TRUE, recursive = TRUE),
+  path = list.files(file.path(s2_dir, "include"), "\\.(h|hpp|cpp)$", full.names = TRUE, recursive = TRUE),
   final_path = str_replace(path, ".*?include/", "src/")
 )
 
@@ -24,7 +24,7 @@ headers <- tibble(
 # there aren't any headers in subdirs, so we don't need to
 # search + replace in any source files
 source_files <- tibble(
-  path = list.files(file.path(proj_dir, "src"), "\\.(h|hpp|c|cpp)$", full.names = TRUE, recursive = TRUE),
+  path = list.files(file.path(s2_dir, "src"), "\\.(h|hpp|c|cpp)$", full.names = TRUE, recursive = TRUE),
   final_path = str_replace(path, ".*?src/", "src/") %>%
     str_replace_all("/", "__") %>%
     str_replace("src__", "src/")
---
+++ src/code.cpp
@@ -0,0 +1,7 @@
+#include <Rcpp.h>
+using namespace Rcpp;
+
+// [[Rcpp::export]]
+void test() {
+  Rcout << "Test!!\n";
+}
