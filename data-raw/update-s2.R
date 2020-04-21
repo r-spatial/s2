bc4c368aa59c3e05fdced6e26eaf398ae59f55e9
Dewey Dunnington <dewey@fishandwhistle.net>
2020-04-21 11:09:49 -0300

    working copy script

--- data-raw/update-s2.R
+++ data-raw/update-s2.R
@@ -9,14 +9,15 @@ unzip("data-raw/s2-source.tar.gz", exdir = "data-raw")
 # make sure the dir exists
 s2_dir <- list.files("data-raw", "^s2geometry-[0-9.]+", include.dirs = TRUE, full.names = TRUE)
 stopifnot(dir.exists(s2_dir), length(s2_dir) == 1)
+src_dir <- file.path(s2_dir, "src/s2")
 
 # headers can be in subdirectories no problem
 # it might make more sense to have these in inst/include,
 # but I can't get that to work...so they are in src/, with
 # the CXXFLAGS=-I../src
 headers <- tibble(
-  path = list.files(file.path(s2_dir, "include"), "\\.(h|hpp|cpp)$", full.names = TRUE, recursive = TRUE),
-  final_path = str_replace(path, ".*?include/", "src/")
+  path = list.files(file.path(s2_dir, "src", "s2"), "\\.(h|hpp|cpp)$", full.names = TRUE, recursive = TRUE),
+  final_path = str_replace(path, ".*?s2/", "inst/include/s2/")
 )
 
 # R doesn't do recursive folders easily
@@ -24,21 +25,19 @@ headers <- tibble(
 # there aren't any headers in subdirs, so we don't need to
 # search + replace in any source files
 source_files <- tibble(
-  path = list.files(file.path(s2_dir, "src"), "\\.(h|hpp|c|cpp)$", full.names = TRUE, recursive = TRUE),
+  path = list.files(file.path(s2_dir, "src", "s2"), "\\.cc$", full.names = TRUE, recursive = TRUE),
   final_path = str_replace(path, ".*?src/", "src/") %>%
     str_replace_all("/", "__") %>%
-    str_replace("src__", "src/")
+    str_replace("^.*?s2__", "src/")
 ) %>%
-  filter(!str_detect(path, "/(apps|tests?)/"))
+  filter(!str_detect(path, "_test\\."))
 
 # clean source dir
 current_source_files <- tibble(path = list.files("src", "\\.(h|hpp|cpp|o)$", full.names = TRUE, recursive = TRUE)) %>%
-  filter(!str_detect(path, "^src/(geoproj-|Rcpp|Makevars|proj_config)"))
+  filter(!str_detect(path, "^src/(libs2-|Rcpp|Makevars)"))
 
 unlink(current_source_files$path)
-unlink("src/proj/", recursive = TRUE)
-stopifnot(file.remove(list.dirs("src")[!str_detect(list.dirs("src"), "^src$")]))
-
+unlink("inst/include/s2", recursive = TRUE)
 
 # create destination dirs
 dest_dirs <- c(
@@ -47,7 +46,7 @@ dest_dirs <- c(
 ) %>%
   dirname() %>%
   unique()
-dest_dirs[!dir.exists(dest_dirs)] %>% walk(dir.create)
+dest_dirs[!dir.exists(dest_dirs)] %>% walk(dir.create, recursive = TRUE)
 
 # copy source files
 stopifnot(
