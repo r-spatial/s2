4b4c2e32cd6441f59a11f1261caf7a8d87c01b01
Dewey Dunnington <dewey@fishandwhistle.net>
2020-05-23 11:31:21 -0300

    fix ring directions of countries dataset

--- data-raw/s2_world_borders.R
+++ data-raw/s2_world_borders.R
@@ -3,6 +3,12 @@ library(sf)
 library(rnaturalearth)
 
 ne <- ne_countries(returnclass = "sf") %>% st_set_precision(1e7)
+
+# write and read to ensure ring direction
+temp_json <- tempfile(fileext = ".geojson")
+write_sf(ne, temp_json)
+ne <- read_sf(temp_json, check_ring_dir = TRUE)
+
 ne_wkb <- st_as_binary(ne$geometry, EWKT = TRUE) %>% wk::wkb()
 s2_data_world_borders <- as.data.frame(tibble::tibble(name = ne$admin, geometry = ne_wkb))
 
