37182bfd0c535e4547729716454a1da0d5cec59a
Dewey Dunnington <dewey@fishandwhistle.net>
2020-05-23 20:55:39 -0300

    add continent column

--- data-raw/s2_world_borders.R
+++ data-raw/s2_world_borders.R
@@ -19,7 +19,13 @@ ne$geometry[[ant_ind]][] <- lapply(ne$geometry[[ant_ind]], function(ply) {
 })
 
 ne_wkb <- st_as_binary(ne$geometry, EWKT = TRUE) %>% wk::wkb()
-s2_data_world_borders <- as.data.frame(tibble::tibble(name = ne$admin, geometry = ne_wkb))
+s2_data_world_borders <- as.data.frame(
+  tibble::tibble(
+    name = ne$admin,
+    continent = ne$continent,
+    geometry = ne_wkb
+  )
+)
 
 # check to see if this will fly in libs2
 wb_s2 <- libs2::s2geography(ne_wkb)
