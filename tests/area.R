4f374ee798128db27fb897444f80bcd82e78286d
Edzer Pebesma <edzer.pebesma@uni-muenster.de>
2020-06-09 11:22:46 +0200

    address NOTE; push windows 386

--- tests/area.R
+++ tests/area.R
@@ -1,10 +1,10 @@
 library(libs2)
-s2_area(
-  s2_union(
+s2_set_snaplevel(29)
+u = s2_union(
    "POLYGON ((0 0, 10 0, 10 10, 0 10, 0 0))",
    "POLYGON ((5 5, 15 5, 15 15, 5 15, 5 5))"
-  ), radius = 1
 )
+s2_area(u, radius = 1)
 s2_area("POLYGON ((0 0, 10 0, 10 10, 0 10, 0 0))", radius = 1) +
 s2_area("POLYGON ((5 5, 15 5, 15 15, 5 15, 5 5))", radius = 1) -
 s2_area("POLYGON ((5 5, 10 5, 10 15, 5 10, 5 5))", radius = 1)
