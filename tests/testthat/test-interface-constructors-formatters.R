3585cacd07776b39257842499d4aeab7886e639a
Edzer Pebesma <edzer.pebesma@uni-muenster.de>
2020-06-12 18:09:49 +0200

    add tests for s2_asbinary

--- tests/testthat/test-interface-constructors-formatters.R
+++ tests/testthat/test-interface-constructors-formatters.R
@@ -1,12 +1,12 @@
 
-test_that("st_astext() works", {
+test_that("s2_astext() works", {
   expect_identical(
     s2_astext("POINT (0.1234567890123456 0.1234567890123456)"),
     "POINT (0.1234567890123456 0.1234567890123456)"
   )
 })
 
-test_that("st_asbinary() works", {
+test_that("s2_asbinary() works", {
   expect_identical(
     s2_asbinary("POINT (0 0)", endian = 0),
     list(
@@ -20,4 +20,9 @@ test_that("st_asbinary() works", {
   )
 })
 
-
+test_that("s2_asbinary works on (multi)polygons", {
+	w = s2data_countries()
+	b = s2_asbinary(w)
+	all.equal(object.size(b), structure(183840, class = 'object_size'))
+	all.equal(length(b), 177L)
+})
