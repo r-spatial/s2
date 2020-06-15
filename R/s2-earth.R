cabe9c276e53df0a366834d466f333deba24567f
Dewey Dunnington <dewey@fishandwhistle.net>
2020-06-14 21:49:01 -0300

    add ref for earth radius

--- R/s2-earth.R
+++ R/s2-earth.R
@@ -1,7 +1,16 @@
 
 #' Earth constants
 #'
+#' According to Yoder (1995), the radius of the earth is
+#' 6371.01 km.
+#'
 #' @export
+#'
+#' @references
+#' Yoder, C.F. 1995. "Astrometric and Geodetic Properties of Earth and the
+#' Solar System" in Global Earth Physics, A Handbook of Physical Constants,
+#' AGU Reference Shelf 1, American Geophysical Union, Table 2.
+#' <https://doi.org/10.1029/RF001p0001>
 #'
 #' @examples
 #' s2_earth_radius_meters()
