c54d348f872ed7dc7d8998553360d71b558ef0fe
Dewey Dunnington <dewey@fishandwhistle.net>
2020-06-15 10:38:44 -0300

    try to fix warnings on MacOS, R 4.0

--- data-raw/update-s2.R
+++ data-raw/update-s2.R
@@ -73,6 +73,7 @@ print_next <- function() {
     "inst/include/s2/util/coding/coder.h[454]: ",
     "Replace call to memset() with loop over pointers ->reset() method"
   )
+  cli::cat_bullet("Replace the ABSL_DEPRECATED macro with a blank macro")
   cli::cat_bullet("Replace `abort()` with `cpp_compat_abort()`")
   cli::cat_bullet("Replace `cerr`/`cout` with `cpp_compat_cerr`/`cpp_compat_cout`")
   cli::cat_bullet("Replace `srandom()` with `cpp_compat_srandom()`")
