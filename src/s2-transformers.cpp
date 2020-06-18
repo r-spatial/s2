a36238da87edade3e068a58dfa3e66a2c640a55a
Dewey Dunnington <dewey@fishandwhistle.net>
2020-06-18 13:35:44 -0300

    consoliate options code

--- src/s2-transformers.cpp
+++ src/s2-transformers.cpp
@@ -23,6 +23,80 @@
 #include <Rcpp.h>
 using namespace Rcpp;
 
+// This class wraps several concepts in the S2BooleanOperation, 
+// and S2Layer, parameterized such that these can be specified from R
+class GeographyOperationOptions {
+public:
+  int polygonModel;
+  int polylineModel;
+  int snapLevel;
+
+  // deaults: use S2 defaults
+  GeographyOperationOptions(): polygonModel(-1), polylineModel(-1), snapLevel(-1) {}
+
+  // 0 = open, 1 = semi_open, 2 = closed
+  void setPolygonModel(int model) {
+    this->polygonModel = model;
+  }
+
+  // 0 = open, 1 = semi_open, 2 = closed
+  void setPolylineModel(int model) {
+    this->polylineModel = model;
+  }
+
+  // this refers to the cell level, not the rounding
+  // precision
+  void setSnapLevel(int snapLevel) {
+    this->snapLevel = snapLevel;
+  }
+
+  // build options for passing this to the S2BooleanOperation
+  S2BooleanOperation::Options booleanOperationOptions() {
+    S2BooleanOperation::Options options;
+    if (this->polygonModel >= 0) {
+      options.set_polygon_model(getPolygonModel(this->polygonModel));
+    }
+
+    if (this->polylineModel >= 0) {
+      options.set_polyline_model(getPolylineModel(this->polylineModel));
+    }
+
+    if (this->snapLevel > 0) {
+      options.set_snap_function(s2builderutil::S2CellIdSnapFunction(this->snapLevel));
+    }
+
+    return options;
+  }
+
+  static S2BooleanOperation::PolygonModel getPolygonModel(int model) {
+    switch (model) {
+      case 0: return S2BooleanOperation::PolygonModel::OPEN;
+      case 1: return S2BooleanOperation::PolygonModel::SEMI_OPEN;
+      case 2: return S2BooleanOperation::PolygonModel::CLOSED;
+      default:
+        // # nocov start
+        std::stringstream err;
+        err << "Invalid value for polygon model: " << model;
+        stop(err.str());
+        // # nocov end
+    }
+  }
+
+  static S2BooleanOperation::PolylineModel getPolylineModel(int model) {
+    switch (model) {
+      case 0: return S2BooleanOperation::PolylineModel::OPEN;
+      case 1: return S2BooleanOperation::PolylineModel::SEMI_OPEN;
+      case 2: return S2BooleanOperation::PolylineModel::CLOSED;
+      default:
+        // # nocov start
+        std::stringstream err;
+        err << "Invalid value for polyline model: " << model;
+        stop(err.str());
+        // # nocov end
+    }
+  }
+};
+
 std::unique_ptr<Geography> geographyFromLayers(std::vector<S2Point> points,
                                                std::vector<std::unique_ptr<S2Polyline>> polylines,
                                                std::unique_ptr<S2Polygon> polygon) {
@@ -107,15 +181,12 @@ class BooleanOperationOp: public BinaryGeographyOperator<List, SEXP> {
 public:
   BooleanOperationOp(S2BooleanOperation::OpType opType, int model, int snapLevel):
     opType(opType) {
-  
-    if (model >= 0) {
-      this->options.set_polygon_model(get_polygon_model(model));
-      this->options.set_polyline_model(get_polyline_model(model));
+      GeographyOperationOptions options;
+      options.setPolygonModel(model);
+      options.setPolylineModel(model);
+      options.setSnapLevel(snapLevel);
+      this->options = options.booleanOperationOptions();
     }
-    if (snapLevel > 0) {
-      this->options.set_snap_function(s2builderutil::S2CellIdSnapFunction(snapLevel));
-    }
-  }
 
   SEXP processFeature(XPtr<Geography> feature1, XPtr<Geography> feature2, R_xlen_t i) {
     std::unique_ptr<Geography> geography = doBooleanOperation(
@@ -159,8 +230,12 @@ List cpp_s2_sym_difference(List geog1, List geog2, int model, int snapLevel) {
 
 // [[Rcpp::export]]
 List cpp_s2_union_agg(List geog, int model, int snapLevel, bool naRm) {
-  MutableS2ShapeIndex index;
+  GeographyOperationOptions options;
+  options.setPolygonModel(model);
+  options.setPolylineModel(model);
+  options.setSnapLevel(snapLevel);
 
+  MutableS2ShapeIndex index;
   SEXP item;
   for (R_xlen_t i = 0; i < geog.size(); i++) {
     item = geog[i];
@@ -174,21 +249,12 @@ List cpp_s2_union_agg(List geog, int model, int snapLevel, bool naRm) {
     }
   }
 
-  S2BooleanOperation::Options options;
-  if (model >= 0) {
-    options.set_polygon_model(get_polygon_model(model));
-    options.set_polyline_model(get_polyline_model(model));
-  }
-  if (snapLevel > 0) {
-    options.set_snap_function(s2builderutil::S2CellIdSnapFunction(snapLevel));
-  }
-
   MutableS2ShapeIndex emptyIndex;
   std::unique_ptr<Geography> geography = doBooleanOperation(
     &index, 
     &emptyIndex,
     S2BooleanOperation::OpType::UNION, 
-    options
+    options.booleanOperationOptions()
   );
 
   return List::create(Rcpp::XPtr<Geography>(geography.release()));
