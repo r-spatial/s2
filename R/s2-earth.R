61250556fd33c43b5bc4606ad8c123f41229afb6
Edzer Pebesma <edzer.pebesma@uni-muenster.de>
2021-01-04 16:30:08 +0100

    new cran req to encode doi

--- R/s2-earth.R
+++ R/s2-earth.R
@@ -13,7 +13,7 @@
 #' Yoder, C.F. 1995. "Astrometric and Geodetic Properties of Earth and the
 #' Solar System" in Global Earth Physics, A Handbook of Physical Constants,
 #' AGU Reference Shelf 1, American Geophysical Union, Table 2.
-#' <https://doi.org/10.1029/RF001p0001>
+#' \doi{10.1029/RF001p0001}
 #'
 #' @examples
 #' s2_earth_radius_meters()
