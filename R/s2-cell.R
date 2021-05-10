39e439b1f58928d9f9908f1ff84794ba67d5483c
Dewey Dunnington <dewey@fishandwhistle.net>
2021-05-10 20:44:53 -0300

    stub cell methods (#85)

--- R/s2-cell.R
+++ R/s2-cell.R
@@ -83,16 +83,146 @@ format.s2_cell <- function(x, ...) {
 
 #' S2 cell operators
 #'
-#' @param x An [s2_cell()] vector
+#' @param x,y An [s2_cell()] vector
+#' @param level An integer between 0 and 30, inclusive.
+#' @param k An integer between 1 and 4
 #' @export
 #'
 s2_cell_is_valid <- function(x) {
   cpp_s2_cell_is_valid(as_s2_cell(x))
 }
 
+# exporters
+
 #' @rdname s2_cell_is_valid
 #' @export
 s2_cell_to_lnglat <- function(x) {
   lnglat <- cpp_s2_cell_to_lnglat(as_s2_cell(x))
   s2_lnglat(lnglat[[1]], lnglat[[2]])
+}
+
+#' @rdname s2_cell_is_valid
+#' @export
+s2_cell_center <- function(x) {
+
+}
+
+#' @rdname s2_cell_is_valid
+#' @export
+s2_cell_boundary <- function(x) {
+
+}
+
+#' @rdname s2_cell_is_valid
+#' @export
+s2_cell_polygon <- function(x) {
+
+}
+
+#' @rdname s2_cell_is_valid
+#' @export
+s2_cell_vertex <- function(x, k) {
+
+}
+
+#' @rdname s2_cell_is_valid
+#' @export
+s2_cell_edge <- function(x, k) {
+
+}
+
+#' @rdname s2_cell_is_valid
+#' @export
+s2_cell_vertices <- function(x) {
+
+}
+
+# accessors
+
+#' @rdname s2_cell_is_valid
+#' @export
+s2_cell_level <- function(x) {
+
+}
+
+#' @rdname s2_cell_is_valid
+#' @export
+s2_cell_is_leaf <- function(x) {
+
+}
+
+#' @rdname s2_cell_is_valid
+#' @export
+s2_cell_area <- function(x) {
+
+}
+
+#' @rdname s2_cell_is_valid
+#' @export
+s2_cell_area_approx <- function(x) {
+
+}
+
+# binary operators
+
+#' @rdname s2_cell_is_valid
+#' @export
+s2_cell_contains <- function(x, y) {
+
+}
+
+#' @rdname s2_cell_is_valid
+#' @export
+s2_cell_distance <- function(x, y) {
+
+}
+
+#' @rdname s2_cell_is_valid
+#' @export
+s2_cell_boundary_distance <- function(x, y) {
+
+}
+
+#' @rdname s2_cell_is_valid
+#' @export
+s2_cell_max_distace <- function(x, y) {
+
+}
+
+#' @rdname s2_cell_is_valid
+#' @export
+s2_cell_may_intersect <- function(x, y) {
+
+}
+
+# transversers
+
+#' @rdname s2_cell_is_valid
+#' @export
+s2_cell_parent <- function(x, level) {
+
+}
+
+#' @rdname s2_cell_is_valid
+#' @export
+s2_cell_child <- function(x, k) {
+
+}
+
+#' @rdname s2_cell_is_valid
+#' @export
+s2_cell_edge_neighbour <- function(x, k) {
+
+}
+
+#' @rdname s2_cell_is_valid
+#' @export
+s2_cell_vertex_neighbour <- function(x, k) {
+
+}
+
+#' @rdname s2_cell_is_valid
+#' @export
+s2_cell_neighbour <- function(x, k) {
+
 }
