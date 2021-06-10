be81be1bd73ec18066387ee1e502f112b315eb9f
Jeroen Ooms <jeroenooms@gmail.com>
2021-06-10 15:11:03 +0200

    Windows: update openssl + ucrt support

--- tools/winlibs.R
+++ tools/winlibs.R
@@ -1,7 +1,7 @@
 # Build against openssl libraries that were compiled with the Rtools gcc toolchain.
-if(!file.exists("../windows/openssl-1.1.1/include/openssl/ssl.h")){
+if(!file.exists("../windows/openssl-1.1.1k/include/openssl/ssl.h")){
   if(getRversion() < "3.3.0") setInternet2()
-  download.file("https://github.com/rwinlib/openssl/archive/v1.1.1.zip", "lib.zip", quiet = TRUE)
+  download.file("https://github.com/rwinlib/openssl/archive/v1.1.1k.zip", "lib.zip", quiet = TRUE)
   dir.create("../windows", showWarnings = FALSE)
   unzip("lib.zip", exdir = "../windows")
   unlink("lib.zip")
