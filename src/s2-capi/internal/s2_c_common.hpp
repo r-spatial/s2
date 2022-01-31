6a47443106a0cf0b34fa8429a4da8fcbe34baa11
Dewey Dunnington <dewey@fishandwhistle.net>
2022-01-31 07:39:16 -0400

    start on rewriting geography abstraction

---
+++ src/s2-capi/internal/s2_c_common.hpp
@@ -0,0 +1,130 @@
+
+#include <stdint.h>
+#include <stdexcept>
+#include <string>
+
+#include "s2/s2polygon.h"
+#include "s2/s2polyline.h"
+
+class S2CAPIError: public std::runtime_error {
+public:
+    S2CAPIError(std::string what): std::runtime_error(what.c_str()) {}
+};
+
+
+// An S2Geography is an abstraction of S2 types that is designed to closely match
+// the scope of a GEOS Geometry. Its methods are limited to those needed to
+// implement C API functions. From an S2 perspective, an S2Geography is
+// an S2Region that can be represented by zero or more S2Shape objects.
+// Current implementations of S2Geography own their data (i.e., the
+// coordinate vectors and underlying S2 objects), however, the interface is
+// designed to allow future abstractions where this is not the case.
+class S2Geography {
+public:
+
+    // The number of S2Shape objects needed to represent this S2Geography
+    virtual int num_shapes() = 0;
+
+    // Returns the given S2Shape (where 0 <= id < num_shapes()). The
+    // caller retains ownership of the S2Shape but the data pointed to
+    // by the object requires that the underlying S2Geography outlives
+    // the returned object.
+    virtual std::unique_ptr<S2Shape> Shape(int id) = 0;
+
+    // Returns an S2Region that represents the object. The caller retains
+    // ownership of the S2Region but the data pointed to by the object
+    // requires that the underlying S2Geography outlives the returned
+    // object.
+    virtual std::unique_ptr<S2Region> Region() = 0;
+
+    // Adds an unnormalized set of S2CellIDs to `cell_ids`. This is intended
+    // to be faster than using Region().GetCovering() directly and to
+    // return a small number of cells that can be used to compute a possible
+    // intersection quickly.
+    virtual void GetCellUnionBound(std::vector<S2CellId>* cell_ids);
+};
+
+
+// capi typedef start
+typedef struct s2_geography_t s2_geography_t;
+// capi typedef end
+
+
+// An S2Geography representing zero or more points using a std::vector<S2Point>
+// as the underlying representation.
+class S2GeographyOwningPoint: public S2Geography {
+public:
+    S2GeographyOwningPoint() {}
+    S2GeographyOwningPoint(S2Point point): points_(1) { points_.push_back(point); }
+    S2GeographyOwningPoint(std::vector<S2Point> points): points_(std::move(points)) {}
+
+    int num_shapes() { return 1; }
+    std::unique_ptr<S2Shape> Shape(int id);
+    std::unique_ptr<S2Region> Region();
+    void GetCellUnionBound(std::vector<S2CellId>* cell_ids);
+
+private:
+    std::vector<S2Point> points_;
+};
+
+
+// An S2Geography representing zero or more polylines using the S2Polyline class
+// as the underlying representation.
+class S2GeographyOwningPolyline: public S2Geography {
+public:
+    S2GeographyOwningPolyline() {}
+    S2GeographyOwningPolyline(std::vector<std::unique_ptr<S2Polyline>> polylines):
+        polylines_(std::move(polylines)) {}
+
+    int num_shapes();
+    std::unique_ptr<S2Shape> Shape(int id);
+    std::unique_ptr<S2Region> Region();
+    void GetCellUnionBound(std::vector<S2CellId>* cell_ids);
+
+private:
+    std::vector<std::unique_ptr<S2Polyline>> polylines_;
+};
+
+
+// An S2Geography representing zero or more polygons using the S2Polygon class
+// as the underlying representation. Note that a single S2Polygon (from the S2
+// perspective) can represent zero or more polygons (from the simple features
+// perspective).
+class S2GeographyOwningPolygon: public S2Geography {
+public:
+    S2GeographyOwningPolygon() {}
+    S2GeographyOwningPolygon(std::unique_ptr<S2Polygon> polygon):
+        polygon_(std::move(polygon)) {}
+
+    int num_shapes() { return 1; }
+    std::unique_ptr<S2Shape> Shape(int id);
+    std::unique_ptr<S2Region> Region();
+    void GetCellUnionBound(std::vector<S2CellId>* cell_ids);
+
+private:
+    std::unique_ptr<S2Polygon> polygon_;
+};
+
+
+// An S2Geography wrapping zero or more S2Geography objects. These objects
+// can be used to represent a simple features GEOMETRYCOLLECTION.
+class S2GeographyCollection: public S2Geography {
+public:
+    S2GeographyCollection(std::vector<std::unique_ptr<S2Geography>> features):
+        features_(std::move(features)), num_shapes_(features_.size()),
+        total_shapes_(0) {
+        for (const auto& feature: features_) {
+            num_shapes_.push_back(feature->num_shapes());
+            total_shapes_ += feature->num_shapes();
+        }
+    }
+
+    int num_shapes();
+    std::unique_ptr<S2Shape> Shape(int id);
+    std::unique_ptr<S2Region> Region();
+
+private:
+    std::vector<std::unique_ptr<S2Geography>> features_;
+    std::vector<int> num_shapes_;
+    int total_shapes_;
+};
---
+++ src/s2-capi/s2_c_common.cpp
@@ -0,0 +1,103 @@
+
+#include "s2/s2shape.h"
+#include "s2/s2region.h"
+#include "s2/s2region_union.h"
+
+#include "s2/s2point_region.h"
+#include "s2/s2point_vector_shape.h"
+#include "s2/s2polyline.h"
+#include "s2/s2polygon.h"
+
+#include "s2/mutable_s2shape_index.h"
+#include "s2/s2shape_index_region.h"
+
+#include "internal/s2_c_common.hpp"
+
+
+void S2Geography::GetCellUnionBound(std::vector<S2CellId>* cell_ids) {
+    MutableS2ShapeIndex index;
+    for (int i = 0; i < num_shapes(); i++) {
+        index.Add(Shape(i));
+    }
+
+    MakeS2ShapeIndexRegion<MutableS2ShapeIndex>(&index).GetCellUnionBound(cell_ids);
+}
+
+std::unique_ptr<S2Shape> S2GeographyOwningPoint::Shape(int id) {
+    return absl::make_unique<S2PointVectorShape>(points_);
+}
+
+std::unique_ptr<S2Region> S2GeographyOwningPoint::Region() {
+    auto region = absl::make_unique<S2RegionUnion>();
+    for (const S2Point& point: points_) {
+        region->Add(absl::make_unique<S2PointRegion>(point));
+    }
+
+    return region;
+}
+
+void S2GeographyOwningPoint::GetCellUnionBound(std::vector<S2CellId>* cell_ids) {
+    if (points_.size() < 10) {
+        for (const S2Point& point: points_) {
+            cell_ids->push_back(S2CellId(point));
+        }
+    } else {
+        S2Geography::GetCellUnionBound(cell_ids);
+    }
+}
+
+
+int S2GeographyOwningPolyline::num_shapes() { return polylines_.size(); }
+
+std::unique_ptr<S2Shape> S2GeographyOwningPolyline::Shape(int id) {
+    return absl::make_unique<S2Polyline::Shape>(polylines_[id].get());
+}
+
+std::unique_ptr<S2Region> S2GeographyOwningPolyline::Region() {
+    auto region = absl::make_unique<S2RegionUnion>();
+    for (const auto& polyline: polylines_) {
+        region->Add(std::unique_ptr<S2Region>(polyline->Clone()));
+    }
+    return region;
+}
+
+void S2GeographyOwningPolyline::GetCellUnionBound(std::vector<S2CellId>* cell_ids) {
+    for (const auto& polyline: polylines_) {
+        polyline->GetCellUnionBound(cell_ids);
+    }
+}
+
+std::unique_ptr<S2Shape> S2GeographyOwningPolygon::Shape(int id) {
+    return absl::make_unique<S2Polygon::Shape>(polygon_.get());
+}
+
+std::unique_ptr<S2Region> S2GeographyOwningPolygon::Region() {
+    return std::unique_ptr<S2Polygon>(polygon_->Clone());
+}
+
+void S2GeographyOwningPolygon::GetCellUnionBound(std::vector<S2CellId>* cell_ids) {
+    polygon_->GetCellUnionBound(cell_ids);
+}
+
+
+int S2GeographyCollection::num_shapes() { return total_shapes_; }
+
+std::unique_ptr<S2Shape> S2GeographyCollection::Shape(int id) {
+    int sum_shapes_ = 0;
+    for (int i = 0; i < total_shapes_; i++) {
+        sum_shapes_ += num_shapes_[i];
+        if (id < sum_shapes_) {
+            return features_[i]->Shape(id - sum_shapes_ + num_shapes_[i]);
+        }
+    }
+
+    throw S2CAPIError("shape id out of bounds");
+}
+
+std::unique_ptr<S2Region> S2GeographyCollection::Region() {
+    auto region = absl::make_unique<S2RegionUnion>();
+    for (const auto& feature: features_) {
+        region->Add(feature->Region());
+    }
+    return region;
+}
