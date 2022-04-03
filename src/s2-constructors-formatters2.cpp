f34774e07e633ef5c56a1b5080f03a8ae3362fb9
Dewey Dunnington <dewey@fishandwhistle.net>
2022-04-02 22:37:07 -0300

    start on new constructors

---
+++ src/s2-constructors-formatters2.cpp
@@ -0,0 +1,219 @@
+
+#define R_NO_REMAP
+#include <R.h>
+#include <Rinternals.h>
+
+#include "wk-v1.h"
+#include "s2-geography/constructor.hpp"
+
+
+#define CPP_START                         \
+    char cpp_exception_error[8096];       \
+    memset(cpp_exception_error, 0, 8096); \
+    try {
+
+#define CPP_END                                           \
+    } catch (std::exception& e) {                         \
+        strncpy(cpp_exception_error, e.what(), 8096 - 1); \
+    }                                                     \
+    Rf_error("%s", cpp_exception_error);                  \
+    return R_NilValue;
+
+
+// The other versions of CPP_START and CPP_END stack-allocate the
+// error message buffer, which takes a non-trivial amount of time
+// when done at this scale (at worst 4 times per coordinate). By
+// keeping the buffer in the handler_data struct, we can call C++
+// from every handler method without measurable overhead.
+#define WK_METHOD_CPP_START                     \
+    try {
+
+#define WK_METHOD_CPP_END                                 \
+    } catch (std::exception& e) {                         \
+        strncpy(data->cpp_exception_error, e.what(), 8096 - 1); \
+    }                                                     \
+    Rf_error("%s", data->cpp_exception_error);            \
+    return R_NilValue;
+
+#define WK_METHOD_CPP_END_INT                                 \
+    } catch (std::exception& e) {                         \
+        strncpy(data->cpp_exception_error, e.what(), 8096 - 1); \
+    }                                                     \
+    Rf_error("%s", data->cpp_exception_error);            \
+    return WK_ABORT;
+
+
+typedef struct {
+    s2geography::CollectionConstructor* builder;
+    int coord_size;
+    char cpp_exception_error[8096];
+} builder_handler_t;
+
+int builder_vector_start(const wk_vector_meta_t* meta, void* handler_data) {
+  return WK_CONTINUE;
+}
+
+SEXP builder_vector_end(const wk_vector_meta_t* meta, void* handler_data) {
+  builder_handler_t* data = (builder_handler_t*) handler_data;
+  WK_METHOD_CPP_START
+
+  WK_METHOD_CPP_END
+}
+
+int builder_feature_start(const wk_vector_meta_t* meta, R_xlen_t feat_id, void* handler_data) {
+  builder_handler_t* data = (builder_handler_t*) handler_data;
+  WK_METHOD_CPP_START
+  data->builder->start_feature();
+  WK_METHOD_CPP_END_INT
+}
+
+int builder_feature_null(void* handler_data) {
+  builder_handler_t* data = (builder_handler_t*) handler_data;
+  WK_METHOD_CPP_START
+  // Append NULL
+  return WK_ABORT_FEATURE;
+  WK_METHOD_CPP_END_INT
+}
+
+int builder_feature_end(const wk_vector_meta_t* meta, R_xlen_t feat_id, void* handler_data) {
+  builder_handler_t* data = (builder_handler_t*) handler_data;
+  WK_METHOD_CPP_START
+  std::unique_ptr<s2geography::S2Geography> feat = data->builder->finish_feature();
+  // Append external pointer to feature
+  WK_METHOD_CPP_END_INT
+}
+
+int builder_geometry_start(const wk_meta_t* meta, uint32_t part_id, void* handler_data) {
+  builder_handler_t* data = (builder_handler_t*) handler_data;
+  WK_METHOD_CPP_START
+
+  auto geometry_type = static_cast<s2geography::util::GeometryType>(meta->geometry_type);
+
+  int32_t size;
+  if (meta->size == WK_SIZE_UNKNOWN) {
+      size = -1;
+  } else {
+      size = meta->size;
+  }
+
+  if (meta->flags & WK_FLAG_HAS_Z && meta->flags & WK_FLAG_HAS_M) {
+    data->coord_size = 4;
+  } else if (meta->flags & WK_FLAG_HAS_Z) {
+    data->coord_size = 3;
+  } else if (meta->flags & WK_FLAG_HAS_M) {
+    data->coord_size = 3;
+  } else {
+    data->coord_size = 2;
+  }
+
+  data->builder->geom_start(geometry_type, size);
+  return WK_CONTINUE;
+  WK_METHOD_CPP_END_INT
+}
+
+int builder_geometry_end(const wk_meta_t* meta, uint32_t part_id, void* handler_data) {
+  builder_handler_t* data = (builder_handler_t*) handler_data;
+  WK_METHOD_CPP_START
+  data->builder->geom_end();
+  return WK_CONTINUE;
+  WK_METHOD_CPP_END_INT
+}
+
+int builder_ring_start(const wk_meta_t* meta, uint32_t size, uint32_t ring_id, void* handler_data) {
+  builder_handler_t* data = (builder_handler_t*) handler_data;
+  WK_METHOD_CPP_START
+
+  if (size == WK_SIZE_UNKNOWN) {
+      data->builder->ring_start(-1);
+  } else {
+      data->builder->ring_start(size);
+  }
+
+  return WK_CONTINUE;
+  WK_METHOD_CPP_END_INT
+}
+
+int builder_ring_end(const wk_meta_t* meta, uint32_t size, uint32_t ring_id, void* handler_data) {
+  builder_handler_t* data = (builder_handler_t*) handler_data;
+  WK_METHOD_CPP_START
+  data->builder->ring_end();
+  return WK_CONTINUE;
+  WK_METHOD_CPP_END_INT
+}
+
+int builder_coord(const wk_meta_t* meta, const double* coord, uint32_t coord_id, void* handler_data) {
+  builder_handler_t* data = (builder_handler_t*) handler_data;
+  WK_METHOD_CPP_START
+  data->builder->coords(coord, 1, data->coord_size);
+  return WK_CONTINUE;
+  WK_METHOD_CPP_END_INT
+}
+
+int builder_error(const char* message, void* handler_data) {
+  Rf_error("%s", message);
+  return WK_ABORT;
+}
+
+void builder_finalize(void* handler_data) {
+  builder_handler_t* data = (builder_handler_t*) handler_data;
+  if (data != nullptr) {
+    free(data);
+  }
+}
+
+void delete_collection_constructor(SEXP xptr) {
+    auto ptr = reinterpret_cast<s2geography::CollectionConstructor*>(R_ExternalPtrAddr(xptr));
+    if (ptr != nullptr) {
+        delete ptr;
+    }
+}
+
+extern "C" SEXP s2_c_geography_writer_new(SEXP oriented_sexp, SEXP check_sexp) {
+  CPP_START
+
+  auto builder = new s2geography::CollectionConstructor();
+  SEXP builder_xptr = PROTECT(R_MakeExternalPtr(builder, R_NilValue, R_NilValue));
+  R_RegisterCFinalizer(builder_xptr, &delete_collection_constructor);
+
+  wk_handler_t* handler = wk_handler_create();
+
+  handler->vector_start = &builder_vector_start;
+  handler->vector_end = &builder_vector_end;
+
+  handler->feature_start = &builder_feature_start;
+  handler->null_feature = &builder_feature_null;
+  handler->feature_end = &builder_feature_end;
+
+  handler->geometry_start = &builder_geometry_start;
+  handler->geometry_end = &builder_geometry_end;
+
+  handler->ring_start = &builder_ring_start;
+  handler->ring_end = &builder_ring_end;
+
+  handler->coord = &builder_coord;
+
+  handler->error = &builder_error;
+
+  handler->finalizer = &builder_finalize;
+
+  builder_handler_t* data = (builder_handler_t*) malloc(sizeof(builder_handler_t));
+  if (data == NULL) {
+    wk_handler_destroy(handler); // # nocov
+    Rf_error("Failed to alloc handler data"); // # nocov
+  }
+
+  data->coord_size = 2;
+  data->builder = builder;
+  memset(data->cpp_exception_error, 0, 8096);
+
+  handler->handler_data = data;
+
+  // include the builder pointer as a tag for this external pointer
+  // which guarnatees that it will not be garbage collected until
+  // this object is garbage collected
+  SEXP handler_xptr = wk_handler_create_xptr(handler, builder_xptr, R_NilValue);
+  UNPROTECT(1);
+  return handler_xptr;
+
+  CPP_END
+}
---
+++ src/s2-geography/constructor.hpp
@@ -0,0 +1,268 @@
+
+# pragma once
+
+#include "geography.h"
+
+namespace s2geography {
+
+namespace util {
+
+enum GeometryType {
+    GEOMETRY_TYPE_UNKNOWN = 0,
+    POINT = 1,
+    LINESTRING = 2,
+    POLYGON = 3,
+    MULTIPOINT = 4,
+    MULTILINESTRING = 5,
+    MULTIPOLYGON = 6,
+    GEOMETRYCOLLECTION = 7
+};
+
+}
+
+class Constructor {
+public:
+
+    virtual ~Constructor() {}
+
+    virtual void geom_start(util::GeometryType geometry_type, int64_t size) {}
+    virtual void ring_start(int32_t size) {}
+
+    virtual void coords(const double* coord, int64_t n, int32_t coord_size) {
+        for (int64_t i = 0; i < n; i++) {
+            S2LatLng pt = S2LatLng::FromDegrees(coord[i * coord_size], coord[i * coord_size + 1]);
+            points_.push_back(pt.ToPoint());
+        }
+    }
+
+    virtual void ring_end() {}
+    virtual void geom_end() {}
+    virtual std::unique_ptr<S2Geography> finish() = 0;
+
+protected:
+    std::vector<S2Point> points_;
+};
+
+class PointConstructor: public Constructor {
+public:
+
+    void geom_start(util::GeometryType geometry_type, int64_t size) {
+        if (size != 0 &&
+            geometry_type != util::GeometryType::POINT &&
+            geometry_type != util::GeometryType::MULTIPOINT &&
+            geometry_type != util::GeometryType::GEOMETRYCOLLECTION) {
+            throw S2GeographyException(
+                "PointConstructor input must be empty, point, multipoint, or collection");
+        }
+
+        points_.reserve(points_.size() + size);
+    }
+
+    void coords(const double* coord, int64_t n, int32_t coord_size) {
+        for (int64_t i = 0; i < n; i++) {
+            if (coord_empty(coord + (i * coord_size), coord_size)) {
+                continue;
+            }
+
+            S2LatLng pt = S2LatLng::FromDegrees(coord[i * coord_size], coord[i * coord_size + 1]);
+            points_.push_back(pt.ToPoint());
+        }
+    }
+
+    std::unique_ptr<S2Geography> finish() {
+        auto result = absl::make_unique<S2GeographyOwningPoint>(std::move(points_));
+        points_.clear();
+        return result;
+    }
+
+private:
+    bool coord_empty(const double* coord, int32_t coord_size) {
+        for (int32_t i = 0; i < coord_size; i++) {
+            if (!std::isnan(coord[i])) {
+                return false;
+            }
+        }
+
+        return true;
+    }
+};
+
+class PolylineConstructor: public Constructor {
+public:
+    void geom_start(util::GeometryType geometry_type, int64_t size) {
+        if (geometry_type == util::GeometryType::MULTILINESTRING ||
+            geometry_type == util::GeometryType::GEOMETRYCOLLECTION) {
+            is_linestring_ = false;
+        } else if (geometry_type == util::GeometryType::LINESTRING ||
+                   size == 0) {
+            points_.reserve(size);
+            is_linestring_ = true;
+        } else {
+            throw S2GeographyException(
+                "PolylineConstructor input must be empty, linestring, multilinestring, or collection");
+        }
+    }
+
+    void geom_end() {
+        if (is_linestring_) {
+            auto polyline = absl::make_unique<S2Polyline>(std::move(points_));
+            polylines_.push_back(std::move(polyline));
+            points_.clear();
+            is_linestring_ = false;
+        }
+    }
+
+    std::unique_ptr<S2Geography> finish() {
+        auto result = absl::make_unique<S2GeographyOwningPolyline>(std::move(polylines_));
+        polylines_.clear();
+        return result;
+    }
+
+private:
+    bool is_linestring_;
+    std::vector<std::unique_ptr<S2Polyline>> polylines_;
+};
+
+class PolygonConstructor: public Constructor {
+public:
+    class Options {
+    public:
+        Options() : oriented_(false), check_(true) {}
+        bool oriented() { return oriented_; }
+        void set_oriented(bool oriented) { oriented_ = oriented; }
+        bool check() { return check_; }
+        void set_check(bool check) { check_ = check; }
+
+    private:
+        bool oriented_;
+        bool check_;
+    };
+
+    PolygonConstructor(const Options& options): options_(options) {}
+
+    void ring_start(int32_t size) {
+        loops_.clear();
+        if (size > 0) {
+            loops_.reserve(size);
+        }
+    }
+
+    void ring_end() {
+        auto loop = absl::make_unique<S2Loop>(std::move(points_));
+        loops_.push_back(std::move(loop));
+        points_.clear();
+    }
+
+    std::unique_ptr<S2Geography> finish() {
+        auto polygon = absl::make_unique<S2Polygon>();
+        polygon->set_s2debug_override(S2Debug::DISABLE);
+        if (options_.oriented()) {
+            polygon->InitOriented(std::move(loops_));
+        } else {
+            polygon->InitNested(std::move(loops_));
+        }
+
+        loops_.clear();
+        return absl::make_unique<S2GeographyOwningPolygon>(std::move(polygon));
+    }
+
+private:
+    std::vector<std::unique_ptr<S2Loop>> loops_;
+    Options options_;
+};
+
+class CollectionConstructor: public Constructor {
+public:
+    CollectionConstructor(const PolygonConstructor::Options& options = PolygonConstructor::Options()):
+        options_(options), level_(0), polygon_constructor_(options) {}
+
+    void geom_start(util::GeometryType geometry_type, int64_t size) {
+        level_++;
+
+        if (active_constructor_ != nullptr) {
+            active_constructor_->geom_start(geometry_type, size);
+            return;
+        }
+
+        switch (geometry_type) {
+        case util::GeometryType::POINT:
+        case util::GeometryType::MULTIPOINT:
+            active_constructor_ = &point_constructor_;
+            break;
+        case util::GeometryType::LINESTRING:
+        case util::GeometryType::MULTILINESTRING:
+            active_constructor_ = &polyline_constructor_;
+            break;
+        case util::GeometryType::POLYGON:
+        case util::GeometryType::MULTIPOLYGON:
+            active_constructor_ = &polygon_constructor_;
+            break;
+        case util::GeometryType::GEOMETRYCOLLECTION:
+            collection_constructor_ = absl::make_unique<CollectionConstructor>();
+            active_constructor_ = collection_constructor_.get();
+            break;
+        default:
+            throw S2GeographyException("CollectionConstructor: unsupported geometry type");
+        }
+
+        active_constructor_->geom_start(geometry_type, size);
+
+    }
+
+    void ring_start(int32_t size) {
+        active_constructor_->ring_start(size);
+    }
+
+    void coords(const double* coord, int64_t n, int32_t coord_size) {
+        active_constructor_->coords(coord, n, coord_size);
+    }
+
+    void ring_end() {
+        active_constructor_->ring_end();
+    }
+
+    void geom_end() {
+        active_constructor_->geom_end();
+        level_--;
+        if (level_ == 0) {
+            auto feature = absl::make_unique<S2GeographyCollection>(std::move(features_));
+            features_.clear();
+            features_.push_back(std::move(feature));
+            active_constructor_ = nullptr;
+        }
+    }
+
+    std::unique_ptr<S2Geography> finish() {
+        auto result = absl::make_unique<S2GeographyCollection>(std::move(features_));
+        features_.clear();
+        return result;
+    }
+
+    void start_feature() {
+        level_ = 0;
+        active_constructor_ = nullptr;
+    }
+
+    std::unique_ptr<S2Geography> finish_feature() {
+        if (features_.empty()) {
+            return absl::make_unique<S2GeographyCollection>();
+        } else {
+            std::unique_ptr<S2Geography> feature = std::move(features_.back());
+            features_.pop_back();
+            return feature;
+        }
+    }
+
+private:
+    PolygonConstructor::Options options_;
+    int level_;
+    PointConstructor point_constructor_;
+    PolylineConstructor polyline_constructor_;
+    PolygonConstructor polygon_constructor_;
+    std::unique_ptr<CollectionConstructor> collection_constructor_;
+    Constructor* active_constructor_;
+
+    std::vector<std::unique_ptr<S2Geography>> features_;
+};
+
+}
