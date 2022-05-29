cd33fd56f720ba3bc1d8e46d5771d4d4706ff726
Dewey Dunnington <dewey@fishandwhistle.net>
2022-05-29 14:26:14 -0300

    header guard instead of pragma once

--- src/s2geography.h
+++ src/s2geography.h
@@ -1,5 +1,6 @@
 
-#pragma once
+#ifndef S2GEOGRAPHY_H_INCLUDED
+#define S2GEOGRAPHY_H_INCLUDED
 
 #include "s2geography/accessors-geog.h"
 #include "s2geography/accessors.h"
@@ -11,3 +12,5 @@
 #include "s2geography/index.h"
 #include "s2geography/linear-referencing.h"
 #include "s2geography/predicates.h"
+
+#endif
