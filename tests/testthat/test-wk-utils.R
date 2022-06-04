a3edffd939975d22037bc887ef8f11c1df92ee50
Dewey Dunnington <dewey@fishandwhistle.net>
2022-06-04 14:05:59 -0400

    test tessellate on import

--- tests/testthat/test-wk-utils.R
+++ tests/testthat/test-wk-utils.R
@@ -231,3 +231,51 @@ test_that("the s2_trans_point() and s2_trans_lnglat() work", {
     wk::wk_set_crs(lng_lats, NULL)
   )
 })
+
+test_that("s2_geography_writer() with tesselate_tol works", {
+  # using big examples here, so use a tolerance of 100 km (forces
+  # adding at least one point)
+  tol <- 100000 / s2_earth_radius_meters()
+
+  expect_equal(
+    wk::as_xy(
+      wk::wk_handle(
+        wk::xy(0, 0),
+        s2_geography_writer(tessellate_tol = tol)
+      )
+    ),
+    wk::xy(0, 0, crs = wk::wk_crs_longlat())
+  )
+
+  expect_identical(
+    wk::wk_handle(
+      wk::wkt("LINESTRING (0 0, 0 45, -60 45)"),
+      s2_geography_writer(tessellate_tol = tol)
+    ) %>%
+      s2_num_points(),
+    6L
+  )
+
+  expect_identical(
+    wk::wk_handle(
+      wk::wkt("POLYGON ((0 0, 0 45, -60 45, 0 0))"),
+      s2_geography_writer(tessellate_tol = tol)
+    ) %>%
+      s2_num_points(),
+    8L
+  )
+})
+
+test_that("s2_geography_writer() with tesselate_tol works with real data", {
+  tol <- 1000 / s2_earth_radius_meters()
+
+  countries_tes <- wk::wk_handle(
+    s2::s2_data_tbl_countries$geometry,
+    s2_geography_writer(tessellate_tol = tol)
+  )
+
+  expect_true(
+    sum(s2_num_points(countries_tes)) >
+      sum(s2_num_points(s2_data_countries()))
+  )
+})
