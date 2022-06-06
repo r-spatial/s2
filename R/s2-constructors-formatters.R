026df6404e69f96e0c24f848ec9b27fb6103b530
Dewey Dunnington <dewey@fishandwhistle.net>
2022-06-06 07:47:36 -0400

    add arguments for passing through planar edges

--- R/s2-constructors-formatters.R
+++ R/s2-constructors-formatters.R
@@ -16,6 +16,9 @@
 #' @param longitude,latitude Vectors of latitude and longitude
 #' @param wkt_string Well-known text
 #' @param wkb_bytes A `list()` of `raw()`
+#' @param planar Use `TRUE` to force planar edges in import or export.
+#' @param tessellate_tol_m The maximum number of meters to that a point must
+#'   be moved to satisfy the planar edge constraint.
 #' @param feature_id,ring_id Vectors for which a change in
 #'   sequential values indicates a new feature or ring. Use [factor()]
 #'   to convert from a character vector.
@@ -101,35 +104,62 @@ s2_make_polygon <- function(longitude, latitude, feature_id = 1L, ring_id = 1L,
 
 #' @rdname s2_geog_point
 #' @export
-s2_geog_from_text <- function(wkt_string, oriented = FALSE, check = TRUE) {
+s2_geog_from_text <- function(wkt_string, oriented = FALSE, check = TRUE,
+                              planar = FALSE,
+                              tessellate_tol_m = s2_tessellate_tol_default()) {
   attributes(wkt_string) <- NULL
   wkt <- wk::new_wk_wkt(wkt_string, geodesic = TRUE)
   wk::validate_wk_wkt(wkt)
 
   wk::wk_handle(
     wkt,
-    s2_geography_writer(oriented = oriented, check = check)
+    s2_geography_writer(
+      oriented = oriented,
+      check = check,
+      tessellate_tol = if (planar) {
+        tessellate_tol_m / s2_earth_radius_meters()
+      } else {
+        Inf
+      }
+    )
   )
 }
 
 #' @rdname s2_geog_point
 #' @export
-s2_geog_from_wkb <- function(wkb_bytes, oriented = FALSE, check = TRUE) {
+s2_geog_from_wkb <- function(wkb_bytes, oriented = FALSE, check = TRUE,
+                             planar = FALSE,
+                             tessellate_tol_m = s2_tessellate_tol_default()) {
   attributes(wkb_bytes) <- NULL
   wkb <- wk::new_wk_wkb(wkb_bytes)
   wk::validate_wk_wkb(wkb)
   wk::wk_handle(
     wkb,
-    s2_geography_writer(oriented = oriented, check = check)
+    s2_geography_writer(
+      oriented = oriented,
+      check = check,
+      tessellate_tol = if (planar) {
+        tessellate_tol_m / s2_earth_radius_meters()
+      } else {
+        Inf
+      }
+    )
   )
 }
 
 #' @rdname s2_geog_point
 #' @export
-s2_as_text <- function(x, precision = 16, trim = TRUE) {
+s2_as_text <- function(x, precision = 16, trim = TRUE,
+                       planar = FALSE,
+                       tessellate_tol_m = s2_tessellate_tol_default()) {
   wkt <- wk::wk_handle(
     as_s2_geography(x),
-    wk::wkt_writer(precision = precision, trim = trim)
+    wk::wkt_writer(precision = precision, trim = trim),
+    s2_tessellate_tol = if (planar) {
+      tessellate_tol_m / s2_earth_radius_meters()
+    } else {
+      Inf
+    }
   )
 
   attributes(wkt) <- NULL
@@ -138,9 +168,25 @@ s2_as_text <- function(x, precision = 16, trim = TRUE) {
 
 #' @rdname s2_geog_point
 #' @export
-s2_as_binary <- function(x, endian = wk::wk_platform_endian()) {
+s2_as_binary <- function(x, endian = wk::wk_platform_endian(),
+                         planar = FALSE,
+                         tessellate_tol_m = s2_tessellate_tol_default()) {
   structure(
-    wk::wk_handle(as_s2_geography(x), wk::wkb_writer(endian = endian)),
+    wk::wk_handle(
+      as_s2_geography(x),
+      wk::wkb_writer(endian = endian),
+      s2_tessellate_tol = if (planar) {
+        tessellate_tol_m / s2_earth_radius_meters()
+      } else {
+        Inf
+      }
+    ),
     class = "blob"
   )
+}
+
+#' @rdname s2_geog_point
+#' @export
+s2_tessellate_tol_default <- function() {
+  100
 }
