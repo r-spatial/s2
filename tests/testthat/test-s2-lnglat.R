479585db55593ae4b9da2ed3c1d9702439aef88d
Hadley Wickham <hadley@posit.co>
2023-04-14 08:43:15 -0500

    Fix test for dev waldo (#226)
    Which now correctly distinguishes between NaN and NA

--- tests/testthat/test-s2-lnglat.R
+++ tests/testthat/test-s2-lnglat.R
@@ -12,7 +12,7 @@ test_that("s2_lnglat objects can be created from and converted back to R objects
   )
   expect_identical(
     as_s2_lnglat(s2_point(NA, NA, NA)),
-    s2_lnglat(NaN, NaN)
+    s2_lnglat(NA, NA)
   )
 })
 
