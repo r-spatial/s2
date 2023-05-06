f2ee085b36c78e8d2348189a1112f816033dfefe
Dewey Dunnington <dewey@fishandwhistle.net>
2023-05-06 13:14:54 -0300

    maybe fix gcc 4.8

--- tests/testthat/test-s2-lnglat.R
+++ tests/testthat/test-s2-lnglat.R
@@ -10,9 +10,10 @@ test_that("s2_lnglat objects can be created from and converted back to R objects
     as_s2_lnglat(s2_point(1, 0, 0)),
     s2_lnglat(0, 0)
   )
+
   expect_identical(
-    as_s2_lnglat(s2_point(NA, NA, NA)),
-    s2_lnglat(NA, NA)
+    as_s2_lnglat(s2_point(NaN, NaN, NaN)),
+    s2_lnglat(NaN, NaN)
   )
 })
 
