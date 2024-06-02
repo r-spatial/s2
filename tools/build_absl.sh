e5fa0183414e25daa4b137f761f93fa9ea0a6a9a
Dewey Dunnington <dewey@fishandwhistle.net>
2024-06-02 16:19:31 -0300

    maybe fix substitution

--- tools/build_absl.sh
+++ tools/build_absl.sh
@@ -1,6 +1,14 @@
 
 # https://cran.r-project.org/doc/manuals/r-release/R-exts.html#Using-cmake
 
+if test -z "$MAKE"; then MAKE="`which make`"; fi
+if ${MAKE} --version ; then
+  echo "Using MAKE=$MAKE $MAKEVARS"
+else
+  echo "cmake not found"
+  exit 1
+fi
+
 if test -z "$CMAKE"; then CMAKE="`which cmake`"; fi
 if test -z "$CMAKE"; then CMAKE=/Applications/CMake.app/Contents/bin/cmake; fi
 if ${CMAKE} --version ; then
@@ -25,7 +33,7 @@ CXXFLAGS=`"${R_HOME}/bin/R" CMD config CXX14FLAGS`
 LDFLAGS=`"${R_HOME}/bin/R" CMD config LDFLAGS`
 
 CMAKE_INSTALL_PREFIX="`pwd`/tools/dist"
-CMAKE_PREFIX_PATH="`pwd`/tools/dist/lib/cmake"
+CMAKE_PREFIX_PATH="`pwd`/tools/dist/lib/cmake:$CMAKE_PREFIX_PATH"
 
 build_cmake () {
   if [ ! -d "tools/build/$1" ]; then
@@ -34,14 +42,14 @@ build_cmake () {
 
   cd "tools/build/$1"
 
-  $CMAKE \
+  ${CMAKE} \
     -DCMAKE_BUILD_TYPE=Release \
     -DBUILD_SHARED_LIBS=OFF \
     -DCMAKE_POSITION_INDEPENDENT_CODE=ON \
     -DCMAKE_INSTALL_PREFIX="${CMAKE_INSTALL_PREFIX}" \
     -DCMAKE_PREFIX_PATH="${CMAKE_PREFIX_PATH}" \
     -DCMAKE_OSX_DEPLOYMENT_TARGET=11.3 \
-    -DCMAKE_CXX_STANDARD=14 "${@:2}" \
+    -DCMAKE_CXX_STANDARD=14 \
     "../../vendor/$1" &&
     ${MAKE} ${MAKEVARS} &&
     ${CMAKE} --install .
@@ -49,5 +57,5 @@ build_cmake () {
   cd ../../..
 }
 
-build_cmake abseil-cpp -DABSL_PROPAGATE_CXX_STD=ON
+build_cmake abseil-cpp
 rm -rf tools/build/abseil-cpp
