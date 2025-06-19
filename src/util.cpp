0f42347417571bcae24407cfeb0c3e190c886369
Taras Zakharko <taras.zakharko@gmail.com>
2025-06-19 10:27:27 +0200

    Fixed the copy-paste typo in package namespace caching  code

--- src/util.cpp
+++ src/util.cpp
@@ -8,7 +8,7 @@ SEXP s2_ns_pkg = NULL;
 
 void s2_init_cached_sexps(void) {
   // package namespace environment
-  s2_ns_pkg = PROTECT(R_FindNamespace(PROTECT(Rf_mkString("nanoarrow"))));
+  s2_ns_pkg = PROTECT(R_FindNamespace(PROTECT(Rf_mkString("s2"))));
 
   // mark the cached objects as in use to prevent deallocation
   R_PreserveObject(s2_ns_pkg);
